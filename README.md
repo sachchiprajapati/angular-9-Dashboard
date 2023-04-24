436c2b476664d8e2c8a5c88dd1a99836f77ad5fe
sachchiprajapati <53114801+sachchiprajapati@users.noreply.github.com>
2023-04-24 18:10:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-9-Dashboard
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9-starter-b2pqlh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular';
+
+  constructor() {}
+}
